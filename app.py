c77b9b461e20628de7cd553885e0c6bb400d7353
mariongcl <marion.gic20@gmail.com>
2025-05-04 21:55:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def calculatrice():
+    resultat = None
+    if request.method == 'POST':
+        try:
+            a = float(request.form['a'])
+            b = float(request.form['b'])
+            operation = request.form['operation']
+            if operation == 'add':
+                resultat = a + b
+            elif operation == 'sub':
+                resultat = a - b
+            elif operation == 'mul':
+                resultat = a * b
+            elif operation == 'div':
+                resultat = a / b if b != 0 else 'Erreur : division par zéro'
+        except ValueError:
+            resultat = 'Erreur : entrez des nombres valides'
+    return render_template('index.html', resultat=resultat)
 
 if __name__ == '__main__':
     app.run(debug=True)
